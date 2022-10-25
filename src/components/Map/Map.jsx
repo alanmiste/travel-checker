feb12d7a9a5231c25134841f39ffe3ca8bae35c8
Alan Miste <baranalaan@gmail.com>
2022-10-25 19:28:51 +0200

    Fix Map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -5,23 +5,32 @@ import { LocationOnOutlined } from "@mui/icons-material";
 import Rating from "@mui/material";
 
 import useStyles from './MapStyles';
-
+// const AnyReactComponent = ({ text }) => <div>{text}</div>;
 export default function Map(){
 
+    // const defaultProps = {
+    //     center: {
+    //       lat: 10.99835602,
+    //       lng: 77.01502627
+    //     },
+    //     zoom: 11
+    //   };
+    
     const classes = useStyles();
     const isMobile = useMediaQuery('(min-width:600px)');
     
     const coordinates = { lat: 0, lng: 0 };
     return(
-        <div >
-            <GoogleMapReact bootstrapURLKeys={{key: ''}}
-                            defualtVenter={coordinates}
+        <div style={{ height: '100vh', width: '100%' }}>
+            <GoogleMapReact bootstrapURLKeys={{key: ""}}
+                            defaultCenter={coordinates}
                             center={coordinates}
-                            defualtZoom={14}
+                            defaultZoom={14}
                             margin={[50,50,50,50]}
                             option={''}
                             onChange={''}
-                            onChildClick={''} >
+                            onChildClick={''} 
+                            >
 
             </GoogleMapReact>
         </div>
