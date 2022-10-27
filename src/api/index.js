d789a8f97dc977777695e5259fb177c939168c58
Alan Miste <baranalaan@gmail.com>
2022-10-27 20:26:41 +0200

    Add Travel-advisor header data.

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,25 @@
 import axios from "axios";
 
+const TravelAdvisorUrl = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary';
 
-const getPlacesData = async () =>{
+const options = {
+    params: {
+      bl_latitude: '11.847676',
+      tr_latitude: '12.838442',
+      bl_longitude: '109.095887',
+      tr_longitude: '109.149359',
+    },
+    headers: {
+      'X-RapidAPI-Key': '',
+      'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
+    }
+  };
+
+export const getPlacesData = async () =>{
     try {
-        const response = await axios.get();
+        const { data: {data} } = await axios.get(TravelAdvisorUrl, options);
+        return data;
     } catch (error) {
-        
+        console.log(error);
     }
 }
