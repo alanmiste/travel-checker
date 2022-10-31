4e3ba96b151302b2c6b928f0d8c51c8d548ae6f4
Alan Miste <baranalaan@gmail.com>
2022-10-31 16:30:16 +0100

    Add restaurants card and locationIcons to maps.

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import GoogleMapReact from "google-map-react";
 import { Paper, Typography, useMediaQuery } from "@mui/material";
-import { LocationOnOutlined } from "@mui/icons-material";
+import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
 import Rating from "@mui/material";
 
-export default function Map({setCoordinates, setBounds, coordinates}){
+export default function Map({setCoordinates, setBounds, coordinates, places}){
 
     
-    const isMobile = useMediaQuery('(min-width:600px)');
+    const isDesktop = useMediaQuery('(min-width:600px)');
     
 
     return(
@@ -24,7 +24,28 @@ export default function Map({setCoordinates, setBounds, coordinates}){
                             }}
                             onChildClick={''} 
                             >
-
+                                {places?.map((place, i)=>(
+                                    <div 
+                                    lat={Number(place.latitude)} 
+                                    lng={Number(place.longitude)}
+                                    key={i}>
+                                        {
+                                         !isDesktop ? (
+                                                <LocationOnOutlinedIcon color="primary" fontSize="large" />
+                                            ) : (
+                                                <Paper elevation={3}>
+                                                    <Typography gutterBottom variant="subtitle2">
+                                                        {place.name}
+                                                    </Typography>
+                                                    <img 
+                                                    src={place.photo ? place.photo.images.large.url : 'https://images.unsplash.com/photo-1550966871-3ed3cdb5ed0c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80'}
+                                                    alt={place.name}
+                                                    />
+                                                </Paper>
+                                            )
+                                        }
+                                    </div>
+                                ))}
             </GoogleMapReact>
         </div>
     );
