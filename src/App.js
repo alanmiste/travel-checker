67ba37ccbdc65ba8c0975649123ab1184fd169d7
Alan Miste <baranalaan@gmail.com>
2022-11-05 20:49:00 +0100

    Move type and rating to App.js.

--- src/App.js
+++ src/App.js
@@ -14,6 +14,8 @@ const App = () =>{
     const [childCliked, setChildCliked] = useState(null);
 
     const [isLoading, setIsLoading] = useState(false);
+    const [type, setType] = useState("restaurants");
+    const [rating, setRating] = useState('');
 
     useEffect(()=>{
         navigator.geolocation.getCurrentPosition(({coords: {latitude, longitude}})=>{
@@ -23,12 +25,12 @@ const App = () =>{
 
     useEffect(()=>{
         setIsLoading(true);
-        getPlacesData(bounds.sw, bounds.ne)
+        getPlacesData(type, bounds.sw, bounds.ne)
         .then(data => {
             setPlaces(data);
             setIsLoading(false);
         })
-    },[coordinates, bounds]);
+    },[type, coordinates, bounds]);
 
     return(
         <>
@@ -40,6 +42,10 @@ const App = () =>{
                         places={places}
                         childCliked={childCliked}
                         isLoading= {isLoading}
+                        type= {type}
+                        setType= {setType}
+                        rating= {rating}
+                        setRating= {setRating}
                          />
                 </Grid>
                 <Grid item xs={12} md={8}>
