ae49ba40eaf62edcc571b3ade8aae1fea3bf20bb
Alan Miste <baranalaan@gmail.com>
2022-11-05 21:05:33 +0100

    Filter Places.

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import {getPlacesData} from './api/index'
 const App = () =>{
 
     const [places, setPlaces] = useState([]);
+    const [filteredPlaces, setFilteredPlaces] = useState([]);
     const [coordinates, setCoordinates] = useState({});
     const [bounds, setBounds] = useState({});
     const [childCliked, setChildCliked] = useState(null);
@@ -24,10 +25,16 @@ const App = () =>{
     },[])
 
     useEffect(()=>{
+        const filteredPlaces = places.filter((place)=> place.rating > rating);
+        setFilteredPlaces(filteredPlaces);
+    },[rating]);
+
+    useEffect(()=>{
         setIsLoading(true);
         getPlacesData(type, bounds.sw, bounds.ne)
         .then(data => {
             setPlaces(data);
+            setFilteredPlaces([]);
             setIsLoading(false);
         })
     },[type, coordinates, bounds]);
@@ -39,7 +46,7 @@ const App = () =>{
             <Grid container spacing={3} style={{ width: '100%' }}>
                 <Grid item xs={12} md={4}>
                     <List 
-                        places={places}
+                        places={filteredPlaces.length ? filteredPlaces : places}
                         childCliked={childCliked}
                         isLoading= {isLoading}
                         type= {type}
@@ -53,7 +60,7 @@ const App = () =>{
                     setCoordinates = {setCoordinates}
                     setBounds = {setBounds}
                     coordinates = {coordinates}
-                    places={places}
+                    places={filteredPlaces.length ? filteredPlaces : places}
                     setChildCliked={setChildCliked}
                     />
                 </Grid>
