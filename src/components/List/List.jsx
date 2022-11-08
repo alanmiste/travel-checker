f2f4097644e0266cfa5422b9870465c1c8ac692d
Alan Miste <baranalaan@gmail.com>
2022-11-08 18:58:36 +0100

    Reformat Docs, add empty line to some files.

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -5,18 +5,18 @@ import './List.css';
 // import useStyles from './ListStyles';
 import PlaceDetails from '../PlaceDetails/PlaceDetails';
 
-export default function List({places, childCliked, isLoading, type, setType, rating, setRating}){
+export default function List({ places, childCliked, isLoading, type, setType, rating, setRating }) {
     // const classes = useStyles();
 
-   
+
     const [elementRefs, setElementRefs] = useState([]);
 
-    useEffect(()=>{
-        const refs = Array(places?.length).fill().map((_,i)=> elementRefs[i] || createRef());
+    useEffect(() => {
+        const refs = Array(places?.length).fill().map((_, i) => elementRefs[i] || createRef());
         setElementRefs(refs);
-    },[places])
+    }, [places])
 
-    return(
+    return (
         <div className="listContainer">
             <Typography variant="h4">Restaurants and Hotels around you</Typography>
             {isLoading ? (
@@ -25,35 +25,35 @@ export default function List({places, childCliked, isLoading, type, setType, rat
                 </div>
             ) : (
                 <>
-            <FormControl className="formControl">
-                <InputLabel>Type</InputLabel>
-                <Select value={type} onChange={ e=> setType(e.target.value)}>
-                    <MenuItem value="restaurants">Restaurants</MenuItem>
-                    <MenuItem value="hotels">Hotels</MenuItem>
-                    <MenuItem value="attractions">Attractions</MenuItem>
-                </Select>
-            </FormControl>
-            <FormControl className="formControl">
-                <InputLabel>Rating</InputLabel>
-                <Select value={rating} onChange={ e=> setRating(e.target.value)}>
-                    <MenuItem value={0}>All</MenuItem>
-                    <MenuItem value={3}>Above 3</MenuItem>
-                    <MenuItem value={4}>Above 4</MenuItem>
-                    <MenuItem value={4.5}>Above 4.5</MenuItem>
-                </Select>
-            </FormControl>
+                    <FormControl className="formControl">
+                        <InputLabel>Type</InputLabel>
+                        <Select value={type} onChange={e => setType(e.target.value)}>
+                            <MenuItem value="restaurants">Restaurants</MenuItem>
+                            <MenuItem value="hotels">Hotels</MenuItem>
+                            <MenuItem value="attractions">Attractions</MenuItem>
+                        </Select>
+                    </FormControl>
+                    <FormControl className="formControl">
+                        <InputLabel>Rating</InputLabel>
+                        <Select value={rating} onChange={e => setRating(e.target.value)}>
+                            <MenuItem value={0}>All</MenuItem>
+                            <MenuItem value={3}>Above 3</MenuItem>
+                            <MenuItem value={4}>Above 4</MenuItem>
+                            <MenuItem value={4.5}>Above 4.5</MenuItem>
+                        </Select>
+                    </FormControl>
 
-            <Grid container spaceing={3} className="listGrid">
-                {places?.map((place, index)=>(
-                    <Grid item key={index} xs={12}>
-                        <PlaceDetails 
-                            place={place}
-                            selected={Number(childCliked) === index}
-                            refProp = {elementRefs[index]} />
+                    <Grid container spaceing={3} className="listGrid">
+                        {places?.map((place, index) => (
+                            <Grid item key={index} xs={12}>
+                                <PlaceDetails
+                                    place={place}
+                                    selected={Number(childCliked) === index}
+                                    refProp={elementRefs[index]} />
+                            </Grid>
+                        ))}
                     </Grid>
-                ))}
-            </Grid>
-            </>
+                </>
             )}
         </div>
     );
